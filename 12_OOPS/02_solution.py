e8247fc6e1613476fc691e2580cd2aa1760c2cd3
Saaransh-Mehta <saaransh1621m@gmail.com>
2025-08-17 00:03:17 +0530

    problem solved via property decorator

--- 12_OOPS/02_solution.py
+++ 12_OOPS/02_solution.py
@@ -1,20 +1,30 @@
 class Cars:
     def __init__(self,brand,model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
     def fullName(self):
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.model}"
+    @staticmethod
+    def general_desription():
+        return "Chasis Number is undefined"
+    @property
+    def get_brand(self):
+        return self.__brand
+
 
 class ElectricCar(Cars):
-    def __init__(self,brand,model,battery_size):
-        super().__init__(brand,model)
+    def __init__(self,__brand,model,battery_size):
+        super().__init__(__brand,model)
         self.batterySize = battery_size
     
     def fullStats(self):
         return f"{self.model} by {self.brand} have battery size of {self.batterySize}"
 
 
-ev_car = ElectricCar("Tesla","S Model","100KWH")
-print(ev_car.model)
-print(ev_car.fullName())
-print(ev_car.fullStats())
+# ev_car = ElectricCar("Tesla","S Model","100KWH")
+# print(ev_car.__brand)
+# print(ev_car.fullName())
+# print(ev_car.fullStats())
+new_car = Cars("Toyota","Fortuner")
+print(Cars.general_desription())
+print(new_car.get_brand)
