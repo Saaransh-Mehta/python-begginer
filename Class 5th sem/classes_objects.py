7d07ea244dd8cd6de12d7dbe71708d0c051353fe
Saaransh-Mehta <saaransh1621m@gmail.com>
2025-08-27 10:29:59 +0530

    created new class Student

--- Class 5th sem/classes_objects.py
+++ Class 5th sem/classes_objects.py
@@ -1,29 +1,36 @@
-class ATM:
+class Student:
     def __init__(self):
-        self.pin = ""
-        self.balance = 0
+        self.name = ""
+        self.age=0
+        self.rollNo = 0
+    def options(self):
+        print('''
+            ----------------
+              Following options to perform some operations
 
-    def main(self):
-        user_input = int(input('''
-        HI There welcome to ATM
-        1. Setup pin
-        2. Update balance
-        '''))
-
+              1. Press 1 For Student Registration
+              2. Press 2 For Student Data Deletion
+              3. Press 3 For Print Details
+        ''')
+        user_input = int(input("Enter the value"))
         if user_input == 1:
-            pin_input = input("What is your new pin: ")
-            self.pin = pin_input
+            self.register()
         elif user_input == 2:
-            self.balance = int(input("Enter the money: "))
+            self.delete()
+    
+    def register(self):
+        self.name = input("Enter the name : ")
+        self.age = int(input("Enter the age : "))
+        self.rollNo = int(input("Enter the roll number: "))
 
-    def printStatus(self):
-        user_input = int(input('''
-        Press 1 for balance
-        '''))
-        if user_input == 1:
-            print(self.balance)
+    def print(self):
+        print(f"Student Name : {self.name} , Age : {self.age} , Roll Number : {self.rollNo}")
+    
+    def delete_record(self):
+        del self.name
+        del self.age
+        del self.rollNo
 
 
-pnb = ATM()
-pnb.main()
-pnb.printStatus()
+
+
